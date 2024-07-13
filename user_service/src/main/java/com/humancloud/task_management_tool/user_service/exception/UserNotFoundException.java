05695bd2a7c16fa15161bd140cc58a279ca716ae
akshay-surjuse <akshay1surjuse@gmail.com>
2024-07-13 19:18:59 +0530

    deleting wrong code

--- user_service/src/main/java/com/humancloud/task_management_tool/user_service/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.humancloud.task_management_tool.user_service.exception;
-
-public class UserNotFoundException extends RuntimeException
-{
-	public UserNotFoundException(String message) {
-		super(message);
-	}
-}
