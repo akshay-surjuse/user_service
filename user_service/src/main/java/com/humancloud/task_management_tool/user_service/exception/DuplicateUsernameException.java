44db94e0d22a5ae9828b008ee44830e2321ccf0e
akshay-surjuse <akshay1surjuse@gmail.com>
2024-07-25 10:34:10 +0530

    deleting older version of user_service because of mistakes

--- user_service/src/main/java/com/humancloud/task_management_tool/user_service/exception/DuplicateUsernameException.java
+++
@@ -1,9 +0,0 @@
-package com.humancloud.task_management_tool.user_service.exception;
-
-public class DuplicateUsernameException extends RuntimeException
-{
-	public DuplicateUsernameException(String message) 
-	{
-		super(message);
-	}
-}
